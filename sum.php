e5177a9cb2c21a9ed95867032cac3d1fb9f679d9
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2024-08-14 15:40:47 +0530

    sum of two numbers

---
+++ sum.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+    <form action="" method="post">
+        <label for="name" name="num1"></label>
+        Enter First Number: <input type="number" name="num1" id="num1"/> <br>
+        Enter second Number: <input type="number" name="num2" id="num2"/> <br>
+        <input type="submit" value="sum"/>
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Retrieve the input values
+        $num1 = $_POST['num1'];
+        $num2 = $_POST['num2'];
+
+        // Calculate the sum
+        $sum = $num1 + $num2;
+
+       
+        echo "<h2>The sum of $num1 and $num2 is: $sum</h2>";
+    }
+
+    ?>
+    
+</body>
+</html>
