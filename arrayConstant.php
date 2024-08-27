9bfbeab36c15c5d73dbcab7e25e11e3804737f83
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2024-08-27 10:07:16 +0530

    array constant

---
+++ arrayConstant.php
@@ -0,0 +1,10 @@
+<?php
+    define("COLOURS", [
+        "RED" => "#FF0000",
+        "GREEN" => "#00FF00",
+        "BLUE" => "0000FF"
+    ]);
+    echo "Red colour code is: ".COLOURS["RED"]."<br>";
+    echo "Green colour code is: ".COLOURS["GREEN"]."<br>";
+    echo "Blue colour code is: ".COLOURS["BLUE"]."<br>";
+?>
